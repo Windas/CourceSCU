c9745c8a8520ace7f67850736d135ca1c4ac4009
Windas Jeffery <Wind_as@outlook.com>
2016-11-12 15:05:22 +0800

    Add devise

---
+++ app/helpers/home/visitors_helper.rb
@@ -0,0 +1,2 @@
+module Home::VisitorsHelper
+end
---
+++ test/controllers/home/visitors_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Home::VisitorsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
